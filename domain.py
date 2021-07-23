e40f1674ce9ce6cd8cad7950522e27ca64f329b4
Your Name <keber.amg79@gmail.com>
2021-07-23 17:32:24 -0300

    add class message

--- domain.py
+++ domain.py
@@ -1,32 +1,32 @@
-"""
-class Message()
-	sera uma superclasse que pega 
-	rodape_valida
-	tamanho
-	comando
-	device
-	mensagem
 
-"""
+class Message():
+	def __init__(self, frame):
+		self.msgsize = frame[4]
+		self.command = frame[6]
+
+		if self.command <= 121:
+			self.controler = "guaritaIP"
+		else:
+			self.controler = "ControladoraII"
+	
+	def is_evento(comando):
+		if (comando == 4 or comando == 116):
+			return True
+
+
 def has_header(frame):
-	print(frame[0:3])
 	if (frame[0] == 83 and frame[1] == 84 and frame [2] == 88):
-		return True
-
-def is_evento(comando):
-	if (comando == 4 or comando == 116):
 		return True
 
 def eval_input(frame):
 	if has_header(frame):
-		if (is_evento(frame[6])):
+		message = Message
+		if message.is_evento:
 			print("COMANDO EVENTO")
 			#run_evento
-			#evento = Evento(message]
 		else:
-			#get_commando
-				#run_comando
+			#run_comando
 			pass
 	else:
-		print("COMANDO DESCONHEcIDO")
+		print("Cabecalho Desconhecio")
 
