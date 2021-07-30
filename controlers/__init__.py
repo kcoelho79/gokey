732638ecc8596f5c0dde31b74e75d1e8b373de64
Your Name <keber.amg79@gmail.com>
2021-07-30 17:01:28 -0300

    add modulo controlers

---
+++ controlers/__init__.py
@@ -0,0 +1,10 @@
+import controlers.mg3000
+
+def get_controler(frame):
+    if frame[6] <= 79:
+        controler = mg3000.MG3000()
+        print("MG3000 encontrado")
+    else:
+        controler = "controladoraII"
+        print("controladora II  encontrado")
+    return controler
---
+++ controlers/mg3000.py
@@ -0,0 +1,10 @@
+
+
+class MG3000():
+    def __init__(self):
+        pass
+
+    def whoiam(self):
+        print("whoiam EU SOU O MG3000")
+
+    
